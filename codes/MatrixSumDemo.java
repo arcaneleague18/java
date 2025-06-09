28fe02320c5a521539ae07ce2d7aa49a775a9f3c
arcaneleague18 <erenyeager4m@gmail.com>
2025-06-09 17:26:57 +0530

    others

--- codes/MatrixSumDemo.java
+++ codes/MatrixSumDemo.java
@@ -10,15 +10,6 @@ public class MatrixSumDemo {
         int rows = matrix.length;
         int cols = matrix[0].length;
         
-        // Display the matrix
-        System.out.println("Matrix:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-        
         // Calculate and display sum of each row
         System.out.println("\nSum of each row:");
         for (int i = 0; i < rows; i++) {
