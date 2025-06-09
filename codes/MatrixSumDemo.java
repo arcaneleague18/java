dd16584528892d656e2290766971d39e0e001e9d
Prithvi <mprithvi2005@gmail.com>
2025-06-09 17:30:12 +0530

    MatrixSumDemo.java

--- codes/MatrixSumDemo.java
+++ codes/MatrixSumDemo.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 public class MatrixSumDemo {
     public static void main(String[] args) {
-        // Sample 3x3 matrix
-        int[][] matrix = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
-        
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        
+        Scanner sc = new Scanner(System.in);
+
+        // Take dimensions input
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        // Input matrix elements
+        System.out.println("Enter elements of the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Element [" + (i + 1) + "][" + (j + 1) + "]: ");
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
         // Calculate and display sum of each row
         System.out.println("\nSum of each row:");
         for (int i = 0; i < rows; i++) {
@@ -19,7 +31,7 @@ public class MatrixSumDemo {
             }
             System.out.println("Row " + (i + 1) + ": " + rowSum);
         }
-        
+
         // Calculate and display sum of each column
         System.out.println("\nSum of each column:");
         for (int j = 0; j < cols; j++) {
@@ -29,5 +41,7 @@ public class MatrixSumDemo {
             }
             System.out.println("Column " + (j + 1) + ": " + colSum);
         }
+
+        sc.close();
     }
 }
