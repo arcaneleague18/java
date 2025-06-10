5d7609ad0539f157449430542ab004c68f6ae6e3
arcaneleague18 <erenyeager4m@gmail.com>
2025-06-10 07:31:54 +0530

    Add files via upload

--- codes/applets/DialogBoxExample.java
+++ codes/applets/DialogBoxExample.java
@@ -1,23 +1,24 @@
 import javax.swing.*;
 
-public class DialogBoxExample {
+class DialogBoxExample extends JFrame {
+    DialogBoxExample() {
+        setTitle("Dialog Box Example");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        JButton button = new JButton("Show Dialog");
+        button.setBounds(90, 70, 120, 30);
+        add(button);
+
+        button.addActionListener(e -> 
+            JOptionPane.showMessageDialog(this, "This is a dialog box!", "Dialog", JOptionPane.INFORMATION_MESSAGE)
+        );
+
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
-        // Show a message dialog
-        JOptionPane.showMessageDialog(null, "Welcome to the Dialog Box Demo!");
-
-        // Ask for user input
-        String name = JOptionPane.showInputDialog("What is your name?");
-
-        // Show a greeting message
-        JOptionPane.showMessageDialog(null, "Hello, " + name + "!");
-
-        // Ask for confirmation
-        int option = JOptionPane.showConfirmDialog(null, "Do you want to continue?", "Confirm", JOptionPane.YES_NO_OPTION);
-
-        if (option == JOptionPane.YES_OPTION) {
-            JOptionPane.showMessageDialog(null, "You chose to continue.");
-        } else {
-            JOptionPane.showMessageDialog(null, "You chose not to continue.");
-        }
+        new DialogBoxExample();
     }
 }
